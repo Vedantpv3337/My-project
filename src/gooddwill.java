394cbcfcbbcf47629d425540176089c8e7de8454
Vedant Vyawhare <vedantpv1398@gmail.com>
2024-09-18 22:13:19 +0530

    practise

--- src/gooddwill.java
+++ src/gooddwill.java
@@ -1,6 +1,9 @@
 public class gooddwill {
     public static void main (String[] args){
-        
+        int a = 4;
+                int b = 5;
+                int sum = a + b ;
+        System.out.println(sum);
     }
 
 }
