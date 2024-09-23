91344cc32cce391caf45e6db4c9292a514170345
Vedant Vyawhare <vedantpv1398@gmail.com>
2024-09-23 23:06:33 +0530

    practise

---
+++ src/vpv_01_practise.java
@@ -0,0 +1,18 @@
+public class vpv_01_practise
+{
+    public static void main (String[] args){
+        // Question 1
+       // int a = 5;
+      //  int b = 6;
+        //  int c = 9;
+     //   int sum = a + b + c ;
+       // System.out.println(sum);
+        // Question 2
+        int subject1 = 95;
+        int subject2 = 45;
+        int subject3 = 90;
+        int cgpa = (subject1 + subject2 + subject3)/30;
+        System.out.println(cgpa);
+
+    }
+}
